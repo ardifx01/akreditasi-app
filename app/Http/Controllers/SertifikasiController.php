df73359def868d1b4f5bfb36aa55ef5771c27b09
Ammar Miftahudin Anshori <ammarmiftah.06@gmail.com>
2025-04-17 09:51:58 +0700

    membuat model dan controller untuk tabel di database staff

---
+++ app/Http/Controllers/SertifikasiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sertifikasi;
+use Illuminate\Http\Request;
+
+class SertifikasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sertifikasi $sertifikasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sertifikasi $sertifikasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Sertifikasi $sertifikasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sertifikasi $sertifikasi)
+    {
+        //
+    }
+}
