b9ad62ada6b33902fa5db6854781af6c2d74694f
ASHARUL <masharul51@gmail.com>
2025-04-18 07:29:30 +0700

    add: auth & authorization

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
+            <h1 class="font-bold">403 Forbidden</h1>
+            <p>You don't have permission to access this page.</p>
+        </div>
+    </div>
+</x-app-layout>
